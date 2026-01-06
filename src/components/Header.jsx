1099fe38d08cbbf2ba37ea96167ed350a3824b4f
Olena Kachurina <lenkachurina@gmail.com>
2026-01-06 16:23:10 +0100

    link CV updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
           <span className="icon-spacer"></span>
         </a>
         <a 
-          href="https://drive.google.com/file/d/1fmyJgL9f6kwluTX7xu6t0GO0_neJMDWQ/view?usp=sharing" 
+          href="https://drive.google.com/drive/folders/1F2KisiBQvYgrAeMAXwbm1H6CSUMgR0da" 
           target="_blank" 
           rel="noopener noreferrer" 
           className="header-footer-link header-footer-link-external"
